52a274871e343c042c8bc2f4c783fc71606f653b
aaditya <iamaaditya2002@gmail.com>
2023-03-24 19:09:38 +0530

    first commit

---
+++ Arraylist.java
@@ -0,0 +1,15 @@
+
+public class Arraylist<T> {
+
+    public void add(int i) {
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public int[] toArray(int[] a1) {
+        return null;
+    }
+
+}
